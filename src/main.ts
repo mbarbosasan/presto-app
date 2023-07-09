efe1bb5a48a36548b3320ab2881a29812870ce76
Moises Santos <moisesbarbosa23@gmail.com>
2023-07-09 02:10:36 -0300

    fix: gerenciamento de estado.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {PrismaService} from "./prisma/PrismaService";
 import {json, urlencoded} from "express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
   app.enableCors()
   app.use(json({ limit: '50mb' }));
