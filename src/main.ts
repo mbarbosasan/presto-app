e57ec498db35d2a1a8b9f3242ef202ec2f63e1df
Moises Santos <moisesbarbosa23@gmail.com>
2023-07-09 00:17:04 -0300

    feat: habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {PrismaService} from "./prisma/PrismaService";
+import {json, urlencoded} from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors()
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 }
